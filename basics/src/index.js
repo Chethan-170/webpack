b2423a73729ada5a485c25b3cbd214423fe9e0fe
Chethan-170 <chethan.poojary170@gmail.com>
2025-07-31 14:03:46 +0530

    lesson-1: run modern js without bundler

---
+++ basics/src/index.js
@@ -0,0 +1,14 @@
+import { greet } from "./utils.js";
+
+
+
+const user = {
+  name: 'Chethan',
+  preferences: {
+    theme: 'dark'
+  }
+};
+
+// Modern syntax
+console.log(`User Theme: ${user?.preferences?.theme}`);
+greet(user.name);
